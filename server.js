fe0a71ec26f4bc904b4f4db3bdde55c98910418b
YusufKer <53224370+YusufKer@users.noreply.github.com>
2020-05-18 14:39:37 +0200

    added functionality to register page... added new dependencies

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+
+const users = []
 
 app.set('view-engine','ejs')
+app.use(express.urlencoded({extended:false}))
 
+//Home Route
 app.get('/',(req,res)=>{
     res.render('index.ejs',{name:"Yusuf"})
 })
+
+//Login Route
 app.get('/login',(req,res)=>{
     res.render('login.ejs')
+    console.log("Request made to login page")
 })
+app.post('/login',(req,res)=>{
+    res.render('login.ejs')
+})
+
+//Register Route
 app.get('/register',(req,res)=>{
     res.render('register.ejs')
+    console.log("Request made to Register page")
 })
-app.post('/',(req,res)=>{
-    res.render('index.ejs',{name:"Yusuf"})
+app.post('/register', async (req,res)=>{
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id:Date.now().toString(),
+            name:req.body.name,
+            email:req.body.email,
+            password: hashedPassword
+        })
+    } catch{
+        res.redirect('register')
+    }
+    console.log(users)
 })
 
 app.listen(3000)
